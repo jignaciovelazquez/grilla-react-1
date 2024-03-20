bd118a676c0616b2f3445fad5dcd871b11cb4933
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-19 23:49:44 -0300

    update toolbar WIP  should add background covering the screen

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,15 +1,29 @@
 import styles from "./ToolBar.module.css";
 import { elementodered } from "../data/pasivos";
 import Model from "./model";
-
-const pasivosList = elementodered.map(({id, type}) => {
-  return <Model key={`key${id}`} id={id} type={type} />;
-});
+import { DialogEditElement } from "./dialogs/DialogEditElement";
+import { useState } from "react";
 
 function ToolBar() {
+  const [isDialogOpen, setModalOpen] = useState(false);
+
+  const pasivosList = elementodered.map(({ id, type }) => {
+    return (
+      <Model
+        key={`key${id}`}
+        id={id}
+        type={type}
+      />
+    );
+  });
+
   return (
     <div>
-      <div className={styles.bar}>{pasivosList}</div>
+      <div className={styles.bar} onClick={() => {
+          console.log('please open the modal');
+          setModalOpen(true);
+        }}>{pasivosList}</div>
+      {isDialogOpen && <DialogEditElement setOpenModal={setModalOpen} />}
     </div>
   );
 }
