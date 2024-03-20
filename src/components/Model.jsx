0acf10a0f672af59cb5c8be0740a74fa4eb3dcc8
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-20 10:54:33 -0300

    refactor several renders andadding update element prop

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,37 +2,32 @@ import style from "./Model.module.css";
 import PropTypes from 'prop-types';
 
 function Model(props) {
-  if (props.type === "pasivo") {
-    return (
-      <div className={style.container}>
-        <div className={style.coupler}>
-          <span>{props.id}</span>
-        </div>
-      </div>
-    );
+
+  const getStyleFigure = () => {
+
+    if (props.type === "pasivo") {
+      return style.coupler;
+    } else if (props.type === "tap") {
+      return style.square; // should be hexagon figure instead
+    } else {
+      return style.triangle;
+    }
   }
-  if (props.type === "tap") {
-    return (
-      <div className={style.container}>
-        <div className={style.square}>
-          <span>{props.id}</span>
-        </div>	
+
+  return (
+    <div className={style.container} onClick={props.updateElementNetwork}>
+      <div className={getStyleFigure()}>
+        <span>{props.id}</span>
       </div>
-    );
-  } else {
-    return (
-      <div className={style.container}>
-        <div className={style.triangle}>
-          <span>{props.id}</span>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 Model.propTypes = {
   type: PropTypes.string,
-  id: PropTypes.string
+  id: PropTypes.string,
+  updateElementNetwork: PropTypes.func
 }
 
 export default Model;
