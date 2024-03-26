68bc2a5e532051618d18ddc6f405a3918e36f7bc
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-26 00:50:59 -0300

    first implementation of context - toolbar

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,12 +1,15 @@
 import styles from "./ToolBar.module.css";
-import { networkElement } from "../data/passives";
 import Model from "./Model";
-
-const modelList = networkElement.map(({ id, type }) => {
-  return <Model key={`key${id}`} id={id} type={type} />;
-});
+import { useContext } from "react";
+import { PassivesContext } from "../context/Contexts";
 
 function ToolBar() {
+  
+  const networkElements = useContext(PassivesContext);
+  const modelList = networkElements.map(({ id, type }) => {
+    return <Model key={`key${id}`} id={id} type={type} />;
+  });
+
   return (
     <>
       <div className={styles.bar}>{modelList}</div>
