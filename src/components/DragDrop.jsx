0b3b06ca4f356e7f76db2647d3c27583b57f61ea
Ignacio Velazquez <jignaciovelazquez@gmail.com>
2024-03-26 12:33:37 -0300

    add useContext into component for use data

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,10 +1,12 @@
-import { networkElement } from "../data/passives";
+import { useContext } from "react";
+import { PassivesContext } from "../context/Contexts";
 import Model from "./Model";
 import { useState } from "react";
 import { useDrop } from "react-dnd";
 import styles from "./DragDrop.module.css";
 
 function DragDrop() {
+  const networkElements = useContext(PassivesContext);
   const [board, setBoard] = useState([]);
 
   const [, drop] = useDrop(() => ({
@@ -16,7 +18,7 @@ function DragDrop() {
   }));
 
   const addImageToBoard = (idx) => {
-    const pictureDrop = networkElement.filter((picture) => idx === picture.id);
+    const pictureDrop = networkElements.filter((picture) => idx === picture.id);
     setBoard((board) => [...board, pictureDrop[0]]);
     //setBoard([pictureDrop[0]]);
   };
