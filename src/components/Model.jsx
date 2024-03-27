1b6f19797c6430dec04a38652dbd19555159e826
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-26 23:21:17 -0300

    refactor: model using a funciton to get te class name value

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,7 +2,7 @@ import style from "./Model.module.css";
 import PropTypes from "prop-types";
 import { useDrag } from "react-dnd";
 
-function Model({ id, type }) {
+function Model({ id, type, updateElementNetwork }) {
   const [, drag] = useDrag(() => ({
     type: "image",
     item: { id: id },
@@ -11,32 +11,27 @@ function Model({ id, type }) {
     }),
   }));
 
-  if (type === "pasivo") {
-    return (
-      <div className={style.container} ref={drag}>
-        <div className={style.coupler}>
-          <span>{id}</span>
-        </div>
-      </div>
-    );
+  const getFigureClassName = (type) => {
+    if (type === "pasivo") {
+      return style.coupler;
+    } else if (type === "tap") {
+      return style.square; // should be hexagon figure instead
+    } else {
+      return style.triangle;
+    }
+  };
+
+  const openDialog = () => {
+    updateElementNetwork(true, type);
   }
-  if (type === "tap") {
-    return (
-      <div className={style.container} ref={drag}>
-        <div className={style.square}>
-          <span>{id}</span>
-        </div>
+
+  return (
+    <div className={style.container} ref={drag} onClick={openDialog}>
+      <div className={getFigureClassName(type)}>
+        <span>{id}</span>
       </div>
-    );
-  } else {
-    return (
-      <div className={style.container} ref={drag}>
-        <div className={style.triangle}>
-          <span>{id}</span>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 Model.propTypes = {
