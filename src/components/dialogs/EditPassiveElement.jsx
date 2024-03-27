7bef907b65a8f1a638ba0cbcbb744bd1a030ccab
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-27 01:24:13 -0300

    component edit passive element showing the data related with model selected

--- src/components/dialogs/EditPassiveElement.jsx
+++ src/components/dialogs/EditPassiveElement.jsx
@@ -1,7 +1,16 @@
+import { useContext, useState } from "react";
 import "./EditPassiveElement.css";
 import PropTypes from "prop-types";
+import { PassivesContext } from "../../context/Contexts";
 
 export function EditPassiveElement({ id, setCloseModal }) {
+
+  const networkElements = useContext(PassivesContext);
+
+  const findElement = networkElements.find((element) => element.id === id);
+
+  const [elementSelected, updateElementSelected] = useState(findElement);
+
   return (
     <div className="modal">
       <div
@@ -12,13 +21,14 @@ export function EditPassiveElement({ id, setCloseModal }) {
       ></div>
       <div className="modal-content">
         <h2>Editar Elemento de Red</h2>
+        <h3>{elementSelected.name}</h3>
         <div>
           <label htmlFor="Attenuation">Atenuación:</label>
-          <input id="Attenuation" type="number" />
+          <input id="Attenuation" type="number" value={elementSelected.atenuacion} />
         </div>
         <div>
           <label htmlFor="Insertion">Inserción:</label>
-          <input id="Insertion" type="number" />
+          <input id="Insertion" type="number" value={elementSelected.insercion} />
         </div>
         <div className="footer">
           <button
