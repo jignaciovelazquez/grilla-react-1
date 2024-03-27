618fe230cefc1a272815941db2d34a828303c678
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-26 23:24:31 -0300

    adding dialogs to app component to test the integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import {DndProvider} from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import ToolBar from "./components/ToolBar";
 import DragDrop from "./components/DragDrop";
 import "./App.css";
-import {PassivesContext} from "./context/Contexts";
-import {networkElement} from "./data/passives";
+import { PassivesContext } from "./context/Contexts";
+import { networkElement } from "./data/passives";
+import { useState } from "react";
+import { EditActiveElement } from "./components/dialogs/EditActiveElement";
+import { EditPassiveElement } from "./components/dialogs/EditPassiveElement";
 
 function App() {
+  const [isDialogActiveOpen, openCloseActiveDialog] = useState(false);
+  const [isDialogPassiveOpen, openClosePassiveDialog] = useState(false);
+
+  const openDialog = (params, typeElement) => {
+    if (typeElement === 'tap' || typeElement === 'pasivo') {
+      openClosePassiveDialog(true);
+    } else {
+      openCloseActiveDialog(params);
+    }
+  }
+
+  if (isDialogActiveOpen || isDialogPassiveOpen) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
     <>
       <NavBar />
       <DndProvider backend={HTML5Backend}>
-        <PassivesContext.Provider value={networkElement} >
-          <ToolBar />
+        <PassivesContext.Provider value={networkElement}>
+          {isDialogActiveOpen && <EditActiveElement setCloseModal={openCloseActiveDialog} />}
+          {isDialogPassiveOpen && <EditPassiveElement setCloseModal={openClosePassiveDialog} />}
+          <ToolBar setOpenCloseModal={openDialog} />
           <section>
             <h1>Arrastre los elementos del Armado</h1>
             <DragDrop />
