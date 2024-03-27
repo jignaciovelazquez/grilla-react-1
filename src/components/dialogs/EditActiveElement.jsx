714f264150d731b767d181f1fab50736a7bb3bec
Jesus Ochoa <jesusochoa266@gmail.com>
2024-03-27 17:28:46 -0300

    feat: active dialog show the values on inputs about model selected

--- src/components/dialogs/EditActiveElement.jsx
+++ src/components/dialogs/EditActiveElement.jsx
@@ -1,8 +1,15 @@
+import { useContext, useState } from "react";
+import { PassivesContext } from "../../context/Contexts";
 import "./EditActiveElement.css";
 import PropTypes from "prop-types";
 
 export function EditActiveElement({ id, setCloseModal }) {
 
+  const networkElements = useContext(PassivesContext);
+
+  const element = networkElements.find((element) => element.id === id);
+
+  const [elementSelected, updateElementSelected] = useState(element);
 
   return (
     <div className="modal">
@@ -14,21 +21,22 @@ export function EditActiveElement({ id, setCloseModal }) {
       ></div>
       <div className="modal-content">
         <h2>Editar Elemento de Red</h2>
+        <h3>{elementSelected.name}</h3>
         <div>
           <label htmlFor="F_hight">F_hight:</label>
-          <input id="F_hight" type="number" />
+          <input id="F_hight" type="number" value={elementSelected.F_hight} />
         </div>
         <div>
           <label htmlFor="F_low">F_low:</label>
-          <input id="F_low" type="number" />
+          <input id="F_low" type="number" value={elementSelected.F_low} />
         </div>
         <div>
           <label htmlFor="R_hight">R_hight:</label>
-          <input id="R_hight" type="number" />
+          <input id="R_hight" type="number" value={elementSelected.R_hight} />
         </div>
         <div>
           <label htmlFor="R_low">R_low:</label>
-          <input id="R_hight" type="number" />
+          <input id="R_hight" type="number" value={elementSelected.R_low} />
         </div>
         <div className="footer">
           <button
