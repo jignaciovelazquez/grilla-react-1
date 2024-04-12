02d12ab8347c3c6c7049fcfd6c6fc4a34ff3f1d1
Jesus Ochoa <jesusochoa266@gmail.com>
2024-04-12 15:46:19 -0300

    feat: toolbar has the container draggable with cable component

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -4,19 +4,21 @@ import { useContext } from "react";
 import { PassivesContext } from "../context/Contexts";
 import { cables } from "../data/cables";
 import { Cable } from "./Figures/Cable";
+import { ContainerDraggable } from "./Containers/ContainerDraggable";
 
 
 function ToolBar() {
-  
   const networkElements = useContext(PassivesContext);
   const modelList = networkElements.map(({ id, type }) => {
     return <Model key={`key${id}`} id={id} type={type} />;
   });
 
-  console.log(cables);
-
-  const cableList = cables.map(({id, color}) => {
-    return <Cable id={id} color={color} />
+  const cableList = cables.map(({id, color}, index) => {
+    return (
+      <ContainerDraggable id={id} key={`key-${index}`}>
+        <Cable color={color} />
+      </ContainerDraggable>
+    );
   });
 
   return (
