fc5e6c863aaef3a0558b88fe32cb9abfb5a82d87
Ignacio Velazquez <108367427+jignaciovelazquez@users.noreply.github.com>
2024-04-25 11:37:19 -0300

    fixed cable icon (#20)
    * fixed cable icon
    
    * fix icon cable, drag element and add icon delete
    
    * one more time

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { useDrop } from "react-dnd";
 import styles from "./DragDrop.module.css";
 import { cables } from "../data/cables";
+import { Cable } from "./Figures/Cable";
+import { Container } from "./Containers/Container";
 
 function DragDrop() {
   const networkElements = useContext(PassivesContext);
@@ -29,15 +31,33 @@ function DragDrop() {
   };
 
   const findCable = (idx) => {
-    return cables.find((cable)=> cable.id === idx);
-  }
+    return cables.find((cable) => cable.id === idx);
+  };
+
+  let dragElement = 1;
 
   return (
     <>
       <div className={styles.Board} ref={drop}>
         {/*console.log("Arreglo de elementos",board)*/}
-        {board.map(({ id, type }, index) => {
-          return <Model key={`key${index}`} id={id} type={type} />;
+
+        {board.map(({ id, type, color }, index) => {
+          if (type != "C") {
+            return (
+              <Model
+                key={`key${index}`}
+                id={id}
+                type={type}
+                dragElement={dragElement}
+              />
+            );
+          } else {
+            return (
+              <Container id={id} key={`key-${index}`}>
+                <Cable color={color} />
+              </Container>
+            );
+          }
         })}
       </div>
     </>
