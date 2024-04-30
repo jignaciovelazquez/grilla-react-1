3368f2597e9fbe57be190fab1160f3b9d16a5e70
Ignacio Velazquez <jignaciovelazquez@gmail.com>
2024-04-30 16:30:23 -0300

    solved values table

--- src/components/ValuesCard.jsx
+++ src/components/ValuesCard.jsx
@@ -4,11 +4,25 @@ function ValuesCard({ sequence }) {
   return (
     <div className={styles.valuesCard}>
       {sequence.map(
-        ({ id, type, name, F_hight, F_low, R_hight, R_low }, index) => {
+        (
+          {
+            id,
+            type,
+            name,
+            F_hight = 0,
+            F_low = 0,
+            R_hight = 0,
+            R_low = 0,
+            attenuation = 0,
+            insertion = 0,
+          },
+          index
+        ) => {
           return (
             <div className={styles.card} key={`key${index}`}>
               <div className={styles.container}>
                 <div>
+                  <p>Abonados</p>
                   <input type="text" value={30} />
                   <label>m</label>
                   <select>
@@ -18,25 +32,40 @@ function ValuesCard({ sequence }) {
                     <option value="value2">X3</option>
                   </select>
                 </div>
-                <p>Abonados</p>
                 <div>
                   <label>{name}</label>
                 </div>
                 <div>
                   <label>H:</label>
-                  <input type="text" value={F_hight} readOnly />
+                  <input
+                    type="text"
+                    value={F_hight != 0 ? F_hight : 40 + attenuation}
+                    readOnly
+                  />
                 </div>
                 <div>
                   <label>L:</label>
-                  <input type="text" value={F_low} readOnly />
+                  <input
+                    type="text"
+                    value={F_low != 0 ? F_low : 40 + attenuation}
+                    readOnly
+                  />
                 </div>
                 <div>
                   <label>RH:</label>
-                  <input type="text" value={R_hight} readOnly />
+                  <input
+                    type="text"
+                    value={R_hight != 0 ? R_hight : 20 + insertion}
+                    readOnly
+                  />
                 </div>
                 <div>
                   <label>RL:</label>
-                  <input type="text" value={R_low} readOnly />
+                  <input
+                    type="text"
+                    value={R_low != 0 ? R_low : 20 + insertion}
+                    readOnly
+                  />
                 </div>
               </div>
             </div>
