b9ee3f271250e326d5a14a29cc24b44dd0f0611b
YisusW <jesusochoa266@gmail.com>
2024-05-04 21:33:31 -0300

    feat: adding the annimation on removable container component

--- src/components/Containers/Removable.jsx
+++ src/components/Containers/Removable.jsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
-import style from "./Removable.module.css";
+import { container, element } from "./Removable.module.css";
 import PropTypes from "prop-types";
 import { IconTrash } from "../UI/IconTrash";
 
 export function Removable({ id, children }) {
 
-  const [hasIcon, toggleIcon] = useState(true);
-
-  const handleOverIn = () => {
-    toggleIcon(true);
-    console.log("over");
-  };
-
-  const handleOverOut = () => {
-    toggleIcon(false);
-    console.log("chao");
-  };
-
   return (
-    <div
-      className={style.menu}
-      onMouseOver={handleOverIn}
-      onMouseOut={handleOverOut}
-    >
-      <div className={style.element}>
-        <IconTrash/>
-      </div>
-      <div className={style.element}>
+    <div className={container}>
+      <div className={element}>
         {children}
         <span>{id}</span>
+      </div>
+      <div className={element}>
+        <IconTrash />
       </div>
     </div>
   );
