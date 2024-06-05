65f59634cd9aa3d40877e658428197ec062a1abc
Jesus Ochoa <jesusochoa266@gmail.com>
2024-06-05 15:07:00 -0300

    Cable input is affecting the belown components (#26)
    * feat: setState of cable input created
    
    * feat: cable affecting the cards added after Cable component
    
    * fix: first valueCard was having a bad padding on container now rollback the changes on module.css file
    
    * cable attribute sign change
    
    ---------
    
    Co-authored-by: Ignacio Velazquez <jignaciovelazquez@gmail.com>

--- src/components/ValuesCard.jsx
+++ src/components/ValuesCard.jsx
@@ -1,10 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PassivesContext } from "../context/Contexts";
 import styles from "./ValuesCard.module.css";
 import Table from "./Table";
 import PropTypes from "prop-types";
+import { cables } from "../data/cables";
+
+const DEFAULT_CABLE_VALUE = cables[0];
 
 function ValuesCard({ sequence }) {
+  const [cableMeters, updateCable] = useState(DEFAULT_CABLE_VALUE.meters);
+  let cableIndex = null;
+
   const networkElements = useContext(PassivesContext);
   let H = 0;
   let L = 0;
@@ -25,10 +31,14 @@ function ValuesCard({ sequence }) {
             RH = updateElement[0].R_hight;
             RL = updateElement[0].R_low;
           } else {
-            H = H - updateElement[0].attenuation;
+            H = H - updateElement[0].attenuation ;
             L = L - updateElement[0].attenuation;
             RH = RH + updateElement[0].insertion;
             RL = RL + updateElement[0].insertion;
+          }
+          if (cableIndex && index > cableIndex) {
+            H = H - (cableMeters * DEFAULT_CABLE_VALUE.attenuation)
+            L = L - (cableMeters * DEFAULT_CABLE_VALUE.attenuation)
           }
           return (
             <div className={styles.card} key={`key${index + 33}`}>
@@ -56,13 +66,19 @@ function ValuesCard({ sequence }) {
             </div>
           );
         } else {
+          cableIndex = index;
           return (
-            <div className={styles.card} key={`key${index + 55}`}>
+            <div className={styles.card} key={`key-${index}-cable`}>
               <div className={styles.container}>
                 <label>{name}</label>
                 <div id="m" className={styles.distance}>
-                  <input type="text" />
-                  <label>m</label>
+                  <input
+                    name="operational-cable"
+                    type="number"
+                    placeholder="Metros"
+                    value={cableMeters}
+                    onChange={e => updateCable(e.target.value)}
+                  />
                 </div>
               </div>
             </div>
